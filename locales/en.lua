edb2e36f206092f18550d80a535e14443d1e5ca3
Kaasheergames <121974307+Kaasheergames@users.noreply.github.com>
2023-04-21 17:56:18 +0200

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,6 @@
+Locales['en'] = {
+	['novehicle'] = '~r~You are not in a vehicle or your vehicle has been blacklisted',
+	['toggletrue'] = 'Streetnames are now ~g~On',
+	['togglefalse'] = 'Streetnames are now ~r~Off',
+	['commandtext'] = 'Toggle streetnames HUD',
+}
---
+++ locales/nl.lua
@@ -0,0 +1,6 @@
+Locales['nl'] = {
+	['novehicle'] = '~r~Je zit momenteel niet in een voertuig, of dit voertuig is niet toegestaan',
+	['toggletrue'] = 'Straatnamen staan nu ~g~aan',
+	['togglefalse'] = 'Straatnamen staan nu ~r~uit',
+	['commandtext'] = 'Zet straatnamen aan of uit',
+}
